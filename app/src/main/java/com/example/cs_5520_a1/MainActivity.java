bc496472e9bd0980bf316fd7a6834cdf5a47d37d
luzixiaoneu <lu.zix@northeastern.edu>
2022-09-25 13:44:23 -0400

    A2 working version

--- app/src/main/java/com/example/cs_5520_a1/MainActivity.java
+++ app/src/main/java/com/example/cs_5520_a1/MainActivity.java
@@ -4,19 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button a = findViewById(R.id.button_a);
+        Button b = findViewById(R.id.button_b);
+        Button c = findViewById(R.id.button_c);
+        Button d = findViewById(R.id.button_d);
+        Button e = findViewById(R.id.button_e);
+        Button f = findViewById(R.id.button_f);
+
+        TextView output = (TextView) findViewById(R.id.pressed_text);
+        output.setText("Pressed: -");
+        a.setOnClickListener(this);
+        b.setOnClickListener(this);
+        c.setOnClickListener(this);
+        d.setOnClickListener(this);
+        e.setOnClickListener(this);
+        f.setOnClickListener(this);
 
     }
 
     public void clickSelfIntro(View view) {
         Toast.makeText(MainActivity.this, "My name is Zixiao Lu, My email is luzixiaoui@gmail.com"
         ,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView output = (TextView) findViewById(R.id.pressed_text);
+        switch (view.getId()) {
+            case R.id.button_a:
+                output.setText("Pressed: A");
+                break;
+            case R.id.button_b:
+                output.setText("Pressed: B");
+                break;
+            case R.id.button_c:
+                output.setText("Pressed: C");
+                break;
+            case R.id.button_d:
+                output.setText("Pressed: D");
+                break;
+            case R.id.button_e:
+                output.setText("Pressed: E");
+                break;
+            case R.id.button_f:
+                output.setText("Pressed: F");
+                break;
+
+        }
     }
 }
